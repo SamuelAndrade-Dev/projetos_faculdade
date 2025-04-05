de9f3fd2de5311861892f3bf3e1e4ac097f3c18c
Samuel Andrade <samuelandrade.spa@gmail.com>
2025-04-04 22:55:12 -0300

    style-geral-pages

--- login/login.js
+++ login/login.js
@@ -2,21 +2,9 @@ function logar() {
     let login = document.getElementById("login").value;
     let senha = document.getElementById("senha").value;
 
-    if (login == "samuelppa" && senha == "samuel") {
-        location.href = "estoque.html";
+    if (login == "samuelppa" && senha == "1234") {
+        location.href = "/home/home.html";
     } else {
         alert("Usuario ou senha incorreto");
-    }
-}
-
-function verificarCheckBox() {
-    let check = document.getElementById("check");
-
-    for (let i = 0; i < check.length; i++) {
-        if (check[i].checked == true) {
-            location.href = "estoque.html";
-        } else {
-            alert("Captcha desmarcado");
-        }
     }
 }
