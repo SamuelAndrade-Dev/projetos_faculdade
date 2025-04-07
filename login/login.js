cf6069fb4003e5de1ce7c4d860fd2ca57167a272
Samuel Andrade <samuelandrade.spa@gmail.com>
2025-04-07 19:37:59 -0300

    style-script

--- login/login.js
+++ login/login.js
@@ -1,10 +1,20 @@
+
 function logar() {
     const login = document.getElementById("login").value;
     const senha = document.getElementById("senha").value;
 
     if (login == "samuelppa" && senha == "1234") {
         location.href = "/home/home.html";
-    } else {
-        alert("Usuario ou senha incorreto");
+    } else if (login == "andre" && senha == "1234") {
+        location.href = "/home/home.html";
+    } else if (login == "pedro" && senha == "1234") {
+        location.href = "/home/home.html";
+    } else if (login == "juliana" && senha == "1234") {
+        location.href = "/home/home.html";
+    } else if (login == "larissaA" && senha == "samuel") {
+        location.href = "/home/home.html";
+    }
+    else {
+        alert("Usuario ou senha incorreto ou usuario não cadastrado!");
     }
 }
