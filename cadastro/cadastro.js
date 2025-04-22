7f4162d383e8a129eac9be6a6abb5fc281dff0f2
Samuel Andrade <samuelandrade.spa@gmail.com>
2025-04-22 20:56:29 -0300

    style-new

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -1,14 +1,5 @@
-function validation() {
-    const name = getElementById("name").value;
-    const nasc = getElementById("nasc").value;
-    const sexo = getElementById("sexo").value;
-    const cpf = getElementById("cpf").value;
-    const tel = getElementById("tel").value;
-    const mail = getElementById("mail").value;
-    const senha = getElementById("senha").value;
-}
+const form = document.querySelectorAll("#form");
 
-if (name.trim() == "") {
-    alert("O campo nome e sobrenome não podem estar vazios!!")
-    return false;
-}
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+});
